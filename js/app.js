ceb90fc0ec6f85d08335ed7b4049c302034efb34
Abid Ali <abid.saeed.ali92@gmail.com>
2020-08-25 22:51:57 +0500

    second commit

--- js/app.js
+++ js/app.js
@@ -3,20 +3,36 @@ var radio = document.getElementById("radio");
 var playBtn = document.getElementById("play-btn");
 var pauseBtn = document.getElementById("pause-btn");
 
-playBtn.addEventListener("click", function () {
-  radio.play();
-  playBtn.style.display = "none";
-  pauseBtn.style.display = "block";
-});
-
-pauseBtn.addEventListener("click", function () {
-  radio.pause();
-  pauseBtn.style.display = "none";
-  playBtn.style.display = "block";
-});
-
 // Vollume Control Function
 var volumeControl = document.getElementById("volume-bar");
 volumeControl.addEventListener("mousemove", function () {
   radio.volume = volumeControl.value / 100;
 });
+
+//
+
+var playPromise = radio.play();
+
+if (playPromise !== undefined) {
+  playPromise
+    .then((_) => {
+      // Automatic playback started!
+      // Show playing UI.
+      // We can now safely pause video...
+      pauseBtn.addEventListener("click", function () {
+        radio.pause();
+        pauseBtn.style.display = "none";
+        playBtn.style.display = "block";
+      });
+      playBtn.addEventListener("click", function () {
+        radio.play();
+        playBtn.style.display = "none";
+        pauseBtn.style.display = "block";
+      });
+      radio.pause();
+    })
+    .catch((error) => {
+      // Auto-play was prevented
+      // Show paused UI.
+    });
+}
